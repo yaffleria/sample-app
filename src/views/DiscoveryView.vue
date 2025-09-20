25206e40335bf3a8822303e2055eca044a7619b4
Jungmin Ji <jungmin.ji@icloud.com>
2025-09-20 12:19:40 +0900

    feat: msw 와 vue-query 구성 및 동작 테스트

--- src/views/DiscoveryView.vue
+++ src/views/DiscoveryView.vue
@@ -1,13 +1,47 @@
 <script setup lang="ts">
   import { useI18n } from 'vue-i18n'
+  import { useQuery } from '@tanstack/vue-query'
+  import getBannerList from '@/api/helper/getBannerList'
+  import { computed } from 'vue'
+  import type { LocalizedText, Banner } from '@/types/discovery'
+  import type { AxiosResponse } from 'axios'
 
-  const { t } = useI18n()
+  const { t, locale } = useI18n()
+  const currentLocale = computed(() => locale.value as LocalizedText)
+
+  const {
+    isLoading: isLoadingBannerList,
+    isError: isErrorBannerList,
+    data: bannerResponse,
+    error,
+  } = useQuery<AxiosResponse<Banner[]>>({
+    queryKey: ['banners'],
+    queryFn: () => getBannerList(),
+  })
+
+  const banners = computed(() => bannerResponse.value?.data)
 </script>
 <template>
   <!-- 메인 컨테이너: 100dvh로 전체 높이, 모바일 퍼스트 -->
   <div class="min-h-[100dvh] bg-gray-50 flex flex-col items-center">
     <!-- 상단 배너 섹션 -->
-    <section class="w-full mb-6">상단 배너 섹션</section>
+    <section class="w-full mb-6">
+      <div v-if="isLoadingBannerList">Loading banners...</div>
+      <div v-else-if="isErrorBannerList">An error occurred: {{ error?.message }}</div>
+      <div v-else-if="banners">
+        <div
+          v-for="banner in banners"
+          :key="banner.name"
+        >
+          <a :href="banner.ctaLinkUrl[currentLocale]">
+            <img
+              :src="banner.imageUrl[currentLocale]"
+              :alt="banner.description[currentLocale]"
+            />
+          </a>
+        </div>
+      </div>
+    </section>
 
     <!-- 즐겨찾기 리스트 섹션 -->
     <section class="w-full mb-6 bg-white rounded-lg shadow-sm">
