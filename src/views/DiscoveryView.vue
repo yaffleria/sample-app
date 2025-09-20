3ee6380c007d65ed3dc3f8950b5c1ed7dbd77857
Jungmin Ji <jungmin.ji@icloud.com>
2025-09-20 11:42:26 +0900

    feat: default layout

--- src/views/DiscoveryView.vue
+++ src/views/DiscoveryView.vue
@@ -4,8 +4,19 @@
   const { t } = useI18n()
 </script>
 <template>
-  <div>
-    <h1>{{ t('dapp_favorite_title') }}</h1>
-    <h1>{{ t('dapp_list_title') }}</h1>
+  <!-- 메인 컨테이너: 100dvh로 전체 높이, 모바일 퍼스트 -->
+  <div class="min-h-[100dvh] bg-gray-50 flex flex-col items-center">
+    <!-- 상단 배너 섹션 -->
+    <section class="w-full mb-6">상단 배너 섹션</section>
+
+    <!-- 즐겨찾기 리스트 섹션 -->
+    <section class="w-full mb-6 bg-white rounded-lg shadow-sm">
+      {{ t('favorites') }}
+    </section>
+
+    <!-- 서비스 리스트 섹션 -->
+    <section class="w-full flex-1 bg-white rounded-lg shadow-sm overflow-hidden">
+      {{ t('services') }}
+    </section>
   </div>
 </template>
