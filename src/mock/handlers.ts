e7f7b4dc58d092afc6f3eaf6600ca24f88dbff29
Jungmin Ji <jungmin.ji@icloud.com>
2025-09-20 13:57:54 +0900

    fix: MSW warning - intercepted a request without a matching req handler

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -1,4 +1,4 @@
-import { http, HttpResponse } from 'msw'
+import { delay, http, HttpResponse } from 'msw'
 import { mockBanners } from '@/mock/data'
 
 export const handlers = [
@@ -6,5 +6,14 @@ export const handlers = [
     // Add 5 second delay to test loading states
     await new Promise((resolve) => setTimeout(resolve, 5000))
     return HttpResponse.json(mockBanners)
+  }),
+  http.get('/*', () => {
+    return
+  }),
+  http.get('/src/*', () => {
+    return
+  }),
+  http.get('/node_modules/*', () => {
+    return
   }),
 ]
