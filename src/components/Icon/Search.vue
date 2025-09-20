d1a6ce891a97088835c80266e8d1c0c7c02220d1
Jungmin Ji <jungmin.ji@icloud.com>
2025-09-20 20:01:10 +0900

    feat: 검색 영역 아이콘

---
+++ src/components/Icon/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    :class="className"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  className?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  className: 'h-5 w-5',
+})
+</script>
+
+<style scoped>
+/* SearchIcon component */
+</style>
--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,2 +1,4 @@
 export { default as BookmarkIcon } from './Bookmark.vue'
 export { default as ExternalLinkIcon } from './ExternalLink.vue'
+export { default as SearchIcon } from './Search.vue'
+export { default as ClearIcon } from './Clear.vue'
